ad8710d512153b45d66e07108d1c9618bed5ac71
LouisDeOliveira <louisdeoliveira@gmail.com>
2022-10-21 23:44:32 +0200

    :fire: :sparkles: play_episode function

--- tmai/training/utils.py
+++ tmai/training/utils.py
@@ -4,8 +4,11 @@ import numpy as np
 import random
 from typing import Generic, Iterable, TypeVar
 
-T = TypeVar("T")
+from tmai.agents.agent import Agent, RandomArrowsAgent
+from tmai.env.TMIClient import ThreadedClient
+from tmai.env.TMNFEnv import TrackmaniaEnv
 
+T = TypeVar("T")
 
 @dataclass
 class Transition:
@@ -14,6 +17,7 @@ class Transition:
     next_state: np.ndarray
     reward: float
 
+Episode = list[Transition]
 
 class Buffer(Generic[T]):
     def __init__(self, capacity=100000):
@@ -32,3 +36,26 @@ class Buffer(Generic[T]):
 
     def __len__(self):
         return len(self.memory)
+
+
+def play_episode(agent:Agent, env:TrackmaniaEnv) -> Episode:
+    env = TrackmaniaEnv(simthread=ThreadedClient(), action_space="arrows")
+    episode = []
+    observation = env.reset()
+    done = False
+    step = 0
+    while not done:
+        prev_obs = observation
+        action = agent.act(observation)
+        print(action)
+        observation, reward, done, info = env.step(action)
+        transition = Transition(prev_obs, action, observation, reward)
+        episode.append(transition)
+        step += 1
+        env.render()
+
+    return episode
+
+if __name__ == "__main__":
+    episode = play_episode(RandomArrowsAgent(), TrackmaniaEnv(simthread=ThreadedClient(), action_space="arrows"))
+    print(len(episode))
