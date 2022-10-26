5d278c5c8deca2e3b4474ffca19d2d7e289ca777
LouisDeOliveira <louisdeoliveira@gmail.com>
2022-10-26 15:55:50 +0200

    :hammer: add speed to obs and new reward

--- tmai/env/TMNFEnv.py
+++ tmai/env/TMNFEnv.py
@@ -34,7 +34,7 @@ class TrackmaniaEnv(Env):
             ArrowsActionSpace if action_space == "arrows" else ControllerActionSpace
         )
         self.observation_space = Box(
-            low=0.0, high=1.0, shape=(n_rays,), dtype=np.float32
+            low=0.0, high=1.0, shape=(n_rays+1,), dtype=np.float32
         )
         
         
@@ -56,7 +56,8 @@ class TrackmaniaEnv(Env):
         self.max_steps = 1000
         self.command_frequency = 50
         self.last_action = None
-
+        self.low_speed_steps = 0
+    
     def step(self, action):
         self.last_action = action
         #plays action 
@@ -69,7 +70,8 @@ class TrackmaniaEnv(Env):
         self.total_reward += self.reward
         self.n_steps += 1
         info = {}
-        return self.viewer.get_obs(), self.reward, done, info
+        time.sleep(self.command_frequency*10e-3)
+        return self.observation, self.reward, done, info
 
     def reset(self):
         print("reset")
@@ -78,10 +80,10 @@ class TrackmaniaEnv(Env):
         self._restart_race()
         self.time = 0
         self.last_action = None
-
+        self.low_speed_steps = 0
         print("reset done")
 
-        return self.viewer.get_obs()
+        return self.observation
 
     def render(self, mode="human"):
         print(f"total reward: {self.total_reward}")
@@ -127,16 +129,40 @@ class TrackmaniaEnv(Env):
     @property
     def speed(self):
         return self.state.display_speed
+    
+    @property
+    def observation(self):
+        return np.concatenate([self.viewer.get_obs(), [self.speed/400]])
 
     @property
     def reward(self):
+        speed = self.speed
         if self.state.time < 3000:
             return 0
 
-        speed = self.state.display_speed
-        speed_reward = speed / 200 if speed > 100 else 0
-        if speed < 10:
-            speed_reward = -10
+        speed_reward = speed / 400
         roll_reward = -abs(self.state.yaw_pitch_roll[2]) / 3.15
         constant_reward = -0.3
-        return speed_reward + roll_reward + constant_reward
+        gas_reward = self.last_action[0]*2
+        
+        
+        if self.last_action[0] < 0:
+            constant_reward -= 10
+            gas_reward = 0
+            
+        if min(self.observation)<0.06:
+            constant_reward -= 100
+            
+        elif 10<speed <100:
+            speed_reward = -1
+            gas_reward = 0
+        
+        elif speed < 10:
+            self.low_speed_steps += 1
+            speed_reward = -5*self.low_speed_steps
+            gas_reward = 0
+            
+        else:
+            self.low_speed_steps = 0
+            
+        return speed_reward + roll_reward + constant_reward + gas_reward
